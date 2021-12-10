9f34a407df8c9f742eeb04bfb2f92ea61aef5437
araujoleonardo310 <araujoleonardo310@gmail.com>
2021-12-10 13:47:20 -0300

    estilizado login

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -2,31 +2,60 @@ import React from 'react';
 
 import './style.css';
 import LottieFile from '../../components/LottieFile';
+import { BsGithub } from 'react-icons/bs';
+import { FaFacebookSquare } from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc';
+import { MdOutlineMoreTime } from 'react-icons/md';
 
 const Login = () => {
   return (
     <div className="container-login">
-      <div className="container-animation">
-        {/* <LottieFile name="workTime" /> */}
-      </div>
-      <div className="container-form">
-        <div className="title">
-          <h2>Melhore sua produtividade</h2>
+      <div className="containers">
+        <div className="container-form">
+          <div className="title">
+            <MdOutlineMoreTime className="moreTime" />
+            <p>Melhore sua produtividade</p>
+          </div>
+          <form className="form">
+            <section className="inputs-wrappers">
+              <div className="wrappers">
+                <input type="email" className="email" />
+                <input type="password" className="password" />
+              </div>
+              <button className="btn login">Login</button>
+            </section>
+            <section className="btns-socials">
+              <button className="btn github">
+                <span className="logo">
+                  <span>
+                    <BsGithub />
+                  </span>
+                  <span>GitHub</span>
+                </span>
+              </button>
+              <button className="btn facebook">
+                <span className="logo">
+                  <span>
+                    <FaFacebookSquare />
+                  </span>
+                  <span>Facebook</span>
+                </span>
+              </button>
+              <button className="btn google">
+                <span className="logo">
+                  <span>
+                    <FcGoogle />
+                  </span>
+                  <span>Google</span>
+                </span>
+              </button>
+            </section>
+          </form>
         </div>
-        <form className="form">
-          <section className='inputs-wrappers'>
-            <div className='wrappers'>
-              <input type="text" />
-              <input type="password" />
-            </div>
-            <button className='btn login'>Login</button>
-          </section>
-          <section className='btns-socials'>
-            <button className='btn github'>GitHub</button>
-            <button className='btn facebook'>Facebook</button>
-            <button className='btn google'>Google</button>
-          </section>
-        </form>
+      </div>
+      <div className="containers lottieAnimation">
+        <LottieFile name="workTime" />
+        <p></p>
       </div>
     </div>
   );
